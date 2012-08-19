5815635f5dd3310be69753a208a3e18d1f5551f8
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-08-19 14:18:15 +0800

    add ejb3 mdb test code

---
+++ sandbox/mdb/src/main/java/com/kylin/ejb/mdb/HelloWorldMDB.java
@@ -0,0 +1,36 @@
+package com.kylin.ejb.mdb;
+
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+
+@MessageDriven(name = "HelloWorldMDB", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/HELLOWORLDMDBQueue"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class HelloWorldMDB implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(HelloWorldMDB.class.toString());
+
+	/**
+	 * @see MessageListener#onMessage(Message)
+	 */
+	public void onMessage(Message rcvMessage) {
+		TextMessage msg = null;
+		try {
+			if (rcvMessage instanceof TextMessage) {
+				msg = (TextMessage) rcvMessage;
+				LOGGER.info("Received Message: " + msg.getText());
+			} else {
+				LOGGER.warning("Message of wrong type: " + rcvMessage.getClass().getName());
+			}
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
