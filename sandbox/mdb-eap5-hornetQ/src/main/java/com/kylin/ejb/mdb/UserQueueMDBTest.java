7bfef45db41e19ce17a5cd93a3da3406d3161313
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-09-05 17:48:26 +0800

    add mdb test code

--- sandbox/mdb-eap5-hornetQ/src/main/java/com/kylin/ejb/mdb/UserQueueMDBTest.java
+++ sandbox/mdb-eap5-hornetQ/src/main/java/com/kylin/ejb/mdb/UserQueueMDBTest.java
@@ -8,7 +8,7 @@ import javax.jms.MessageListener;
 import org.apache.log4j.Logger;
 
 @MessageDriven(name = "UserQueueMDBTest", activationConfig = {
-		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
 		@ActivationConfigProperty(propertyName = "destination", propertyValue = "ddd"),
 		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
 public class UserQueueMDBTest extends MDBTestBase implements MessageListener{
