906948944bf53a7dc5e0392b985f7d8b3e6d95c4
Kylin <kylinsoong.1214@gmail.com>
2013-07-12 15:35:10 +0800

    update invocation code

--- invocation/service/src/main/java/com/kylin/ejb/invocation/service/GreeterBean.java
+++ invocation/service/src/main/java/com/kylin/ejb/invocation/service/GreeterBean.java
@@ -8,6 +8,12 @@ import javax.ejb.Stateless;
 public class GreeterBean implements Greeter {
 
 	public String greet(String user) {
+		System.out.println("GreeterBean invoked");
+		try {
+			Thread.currentThread().sleep(5 * 1000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 		return "Hello " + user + ", have a pleasant day!";
 	}
 
